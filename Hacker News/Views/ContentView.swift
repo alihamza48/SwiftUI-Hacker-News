b22bf805fe27fa90a828744b5904cb27b926b00c
Syed Ali Hamza Naqvi <129213252+alihamza48@users.noreply.github.com>
2024-07-10 13:18:12 +0500

    Added Loader on basis of data ContentView.swift

--- Hacker News/Views/ContentView.swift
+++ Hacker News/Views/ContentView.swift
@@ -14,41 +14,59 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            List(networkManager.posts) {post in
-                NavigationLink(destination: DetailView(url: post.url)) {
-                    HStack {
-                        Text(String(post.points))
-                        Text(post.title)
+            if networkManager.isFetchComplete{
+                List(networkManager.posts) {post in
+                    NavigationLink(destination: DetailView(url: post.url)) {
+                        HStack {
+                            Text(String(post.points))
+                            Text(post.title)
+                        }
                     }
+                    
                 }
-                
+                .navigationTitle("Hacker News")
             }
-            .navigationTitle("Hacker News")
+            else{
+                ZStack{
+                    Color(.systemBackground)
+                        .ignoresSafeArea()
+                        .opacity(0.8)
+                    
+                    ProgressView().progressViewStyle(CircularProgressViewStyle(tint: .black))
+                        .scaleEffect(3)
+                }
+                .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: 900)
+                .opacity(0.8)
+            }
         }
         .onAppear(perform: {
-            startNetworkCall()
+            self.networkManager.fetchData()
+//            startNetworkCall()
         })
         
-        if isLoading{
-            ZStack{
-                Color(.systemBackground)
-                    .ignoresSafeArea()
-                    .opacity(0.8)
-                
-                ProgressView().progressViewStyle(CircularProgressViewStyle(tint: .black))
-                    .scaleEffect(3)
-            }
-            .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: 900)
-            .opacity(0.8)
-        }
+//        {
+//            ZStack{
+//                Color(.systemBackground)
+//                    .ignoresSafeArea()
+//                    .opacity(0.8)
+//                
+//                ProgressView().progressViewStyle(CircularProgressViewStyle(tint: .black))
+//                    .scaleEffect(3)
+//            }
+//            .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: 900)
+//            .opacity(0.8)
+//        }
     }
     
-        func startNetworkCall(){
-            isLoading = true
-            self.networkManager.fetchData()
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1){
-                isLoading = false
-            }
+//        func startNetworkCall(){
+//            isLoading = true
+//            if self.networkManager.fetchData() != nil {
+//                isLoading = false
+//            }
+            
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+//
+//            }
            
         }
 }
