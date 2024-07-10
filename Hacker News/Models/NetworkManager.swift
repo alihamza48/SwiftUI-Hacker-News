4d996a2b03c91b2b41a5b56e17dc758d69a97466
Syed Ali Hamza Naqvi <129213252+alihamza48@users.noreply.github.com>
2024-07-10 13:19:08 +0500

    added boolean state to help for loader NetworkManager.swift

--- Hacker News/Models/NetworkManager.swift
+++ Hacker News/Models/NetworkManager.swift
@@ -10,6 +10,7 @@ import Foundation
 class NetworkManager: ObservableObject{
     
     @Published var posts = [Post]()
+    @Published var isFetchComplete = false
     
     func fetchData(){
         if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") {
@@ -22,10 +23,58 @@ class NetworkManager: ObservableObject{
                            let results = try decoder.decode(Results.self, from: safeData)
                             DispatchQueue.main.async{
                                 self.posts = results.hits
+                                self.isFetchComplete = true
                             }
                         }catch{
                             print(error)
                         }
+                    }
+                }
+                else {
+                    DispatchQueue.main.async {
+                        self.isFetchComplete = true // Set the boolean to true even if there is an error
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
+//
+//  NetworkManager.swift
+//  Hacker News
+//
+//  Created by admin on 7/9/24.
+//
+
+import Foundation
+
+class NetworkManager: ObservableObject{
+    
+    @Published var posts = [Post]()
+    @Published var isFetchComplete = false
+    
+    func fetchData(){
+        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error == nil {
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        do {
+                           let results = try decoder.decode(Results.self, from: safeData)
+                            DispatchQueue.main.async{
+                                self.posts = results.hits
+                                self.isFetchComplete = true
+                            }
+                        }catch{
+                            print(error)
+                        }
+                    }
+                }
+                else {
+                    DispatchQueue.main.async {
+                        self.isFetchComplete = true // Set the boolean to true even if there is an error
                     }
                 }
             }
